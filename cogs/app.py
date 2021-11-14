3c84f0d36eaa8ada8f71df5d7e802e5bc1e54e8e
ja1dan <ajaidan0@gmail.com>
2021-11-13 22:37:26 -0500

    Add download stats to app embed

--- cogs/app.py
+++ cogs/app.py
@@ -12,14 +12,14 @@ from colorthief import ColorThief
 async def get_apps_autocompleter():
     res_apps = []
     async with aiohttp.ClientSession() as session:
-        async with session.get("https://jailbreaks.app/json/apps.json") as resp:
+        async with session.get('https://jailbreaks.app/json/apps.json') as resp:
             if resp.status == 200:
                 data = await resp.text()
                 apps = json.loads(data)
 
                 # try to find an app with the name given in command
                 for d in apps:
-                    name = re.sub(r'\((.*?)\)', "", d["name"])
+                    name = re.sub(r'\((.*?)\)', '', d.get('name'))
                     # get rid of '[ and ']'
                     name = name.replace('[', '')
                     name = name.replace(']', '')
@@ -29,25 +29,37 @@ async def get_apps_autocompleter():
 
     return res_apps
 
-
 async def apps_autocomplete(ctx: AutocompleteContext):
     apps = await get_apps_autocompleter()
     apps.sort()
     return [app for app in apps if app.lower().startswith(ctx.value.lower())][:25]
 
+
+async def get_stats(query):
+    res_stats = []
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.jailbreaks.app/stats/all') as resp:
+            if resp.status == 200:
+                data = await resp.text()
+                stats = json.loads(data).get('stats')
+                res_stats = stats.get(query)
+
+    return res_stats
+
 async def get_apps():
     res_apps = []
     async with aiohttp.ClientSession() as session:
-        async with session.get("https://jailbreaks.app/json/apps.json") as resp:
+        async with session.get('https://jailbreaks.app/json/apps.json') as resp:
             if resp.status == 200:
                 data = await resp.text()
                 res_apps = json.loads(data)
+                
     return res_apps
 
 async def iterate_apps(query) -> dict:
     apps = await get_apps()
     for possibleApp in apps:
-        if possibleApp['name'].lower() == query.lower():
+        if possibleApp.get('name').lower() == query.lower():
             return possibleApp
 
 class App(commands.Cog):
@@ -60,6 +72,7 @@ class App(commands.Cog):
         if app == None:
             await ctx.respond("That app isn't on Jailbreaks.app.", ephemeral=True)
             return
+        stats = await get_stats(query=app.get('name'))
         mainDLLink = f"https://api.jailbreaks.app/{name.replace(' ', '')}"
         allVersions = f"[Latest ({app.get('version')})](https://api.jailbreaks.app/{name.replace(' ', '')})"
         if len(app.get('other_versions')) != 0:
@@ -71,10 +84,9 @@ class App(commands.Cog):
         embed = discord.Embed(title=app.get('name'), color=discord.Color.from_rgb(
             color[0], color[1], color[2]), url=mainDLLink, description=app.get('short-description'))
         embed.set_thumbnail(url=f"https://jailbreaks.app/{app.get('icon')}")
-        embed.add_field(
-            name=f"Download{'' if len(app.get('other_versions')) == 0 else 's'}", value=allVersions, inline=True)
-        embed.add_field(
-            name="Developer", value=f"{('[' + app.get('dev') + '](https://twitter.com/' + app.get('dev') + ')') if app.get('dev').startswith('@') else app.get('dev')}")
+        embed.add_field(name=f"Download{'' if len(app.get('other_versions')) == 0 else 's'}", value=allVersions, inline=True)
+        embed.add_field(name='Developer', value=f"{('[' + app.get('dev') + '](https://twitter.com/' + app.get('dev') + ')') if app.get('dev').startswith('@') else app.get('dev')}", inline=True)
+        embed.add_field(name='Downloads', value=stats)
         embed.set_footer(text='Jailbreaks.app | Made by Jaidan', icon_url='https://avatars.githubusercontent.com/u/37126748')
         await ctx.respond(embed=embed)
 
